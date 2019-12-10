973d201eff5c66f685cb4e5e09c5d29815947c1f
Jonathan Fernandes <xjhofernandes@gmail.com>
2019-12-09 22:51:26 -0300

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace bitNow.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            HttpClient http = new HttpClient();
+            var data = http.GetAsync("https://api.coinbase.com/v2/prices/spot?currency=USD").Result.Content.ReadAsStringAsync().Result;
+            return data;
+        }
+    }
+}
---
+++ Models/BitCoinModel.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bitNow.Models
+{
+    public class BitCoinModel
+    {
+        [JsonProperty("base")]
+        public Int64 Base { get; set; }
+
+        [JsonProperty("currency")]
+        public int Currency { get; set; }
+
+        [JsonProperty("amount")]
+        public string amount { get; set; }
+
+        [JsonProperty("timestamp")]
+        public string Timestamp { get; set; }
+    }
+}
